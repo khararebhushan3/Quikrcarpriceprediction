ef700a011ea2ed0bc5ea88920439b606763a2a85
Bhushan Kharare <khararebhushan3@gmail.com>
2024-03-18 23:21:02 +0530

    Add updateds

--- Modelapp.py
+++ Modelapp.py
@@ -49,8 +49,8 @@ def predict_api():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
 #if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=8080)    
+    #app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)    
